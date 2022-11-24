089304cbf155432c16c274bcc8612dd6e7f9c1fb
nicolids <nicolidss@gmail.com>
2022-11-23 21:35:42 -0300

    inicio montagem desafio

---
+++ src/Carta.java
@@ -0,0 +1,20 @@
+public class Carta {
+    private String nome;
+    private String naipe;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNaipe() {
+        return naipe;
+    }
+
+    public void setNaipe(String naipe) {
+        this.naipe = naipe;
+    }
+}
